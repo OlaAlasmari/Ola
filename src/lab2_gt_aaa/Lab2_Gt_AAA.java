dcfe22e93089c1e3b0572ea11f07c2a2837e99a9
Ola <olaalioa123@gmail.com>
2024-01-31 11:28:32 +0300

    simple print statement

---
+++ src/lab2_gt_aaa/Lab2_Gt_AAA.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2_gt_aaa;
+
+/**
+ *
+ * @author 2206816
+ */
+public class Lab2_Gt_AAA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello GitHun community");
+    }
+    
+}
